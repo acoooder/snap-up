435ccba78bf766fbebc0aee0ea17c0a232070da7
summersky <summer.sky.2014@qq.com>
2020-02-27 23:30:32 +0800

    feat: 增加对于倒计时的判断，防止本地时间和服务器时间的存在的误差

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,44 @@ async function login(page: puppeteer.Page): Promise<boolean> {
   });
 }
 
+async function reload(page: puppeteer.Page, url: string): Promise<void> {
+  await page.goto(url, {
+    waitUntil: 'domcontentloaded'
+  });
+  // await page.waitFor(5000);
+  // const sec = await page.evaluate(() => {
+  //   const text = document.querySelector('.J-time')?.textContent!;
+  //   return parseInt(text.slice(text.indexOf('分') + 1));
+  // });
+  // // 减去各种损耗和误差
+  // await page.waitFor(sec * 1000 - 1000);
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve) => {
+    const listener = async (response: puppeteer.Response) => {
+      // 获取商品预约抢购信息的接口
+      if (response.url().indexOf('https://yushou.jd.com/youshouinfo.action') >= 0) {
+        page.off('response', listener);
+        const text = await response.text();
+        const data = JSON.parse(text.slice(10, text.length - 2));
+        const time = data.d;
+        const getSec = () => {
+          const hour = Math.floor(time / 3600);
+          const min =  Math.floor((time - hour * 3600) / 60);
+          return time - hour * 3600 - min * 60; 
+        };
+        console.log(getSec());
+        
+        await page.waitFor(getSec() * 1000);
+        await page.reload({
+          waitUntil: 'domcontentloaded'
+        });
+        resolve();
+      }
+    };
+    page.on('response', listener);
+  });
+}
+
 async function clickReservation(page: puppeteer.Page): Promise<void> {
   await page.waitForSelector('#btn-reservation');
   return await page.evaluate(() => {
@@ -23,21 +61,16 @@ async function clickReservation(page: puppeteer.Page): Promise<void> {
   });
 }
 
-async function checkInfo(page: puppeteer.Page, url: string): Promise<void> {
-  await page.goto(url, {
-    waitUntil: 'domcontentloaded'
-  });
-
+async function checkInfo(page: puppeteer.Page): Promise<void> {
   return new Promise((resolve) => {
     const listener = async (response: puppeteer.Response) => {
       // 获取商品预约抢购信息的接口
       if (response.url().indexOf('https://yushou.jd.com/youshouinfo.action') >= 0) {
+        page.off('response', listener);
         const text = await response.text();
         const data = JSON.parse(text.slice(10, text.length - 2));
-
         // eslint-disable-next-line no-console
-        console.log(data);
-        page.off('response', listener);
+        console.log(data.d);
         await clickReservation(page);
         resolve(data.info);
       }
@@ -50,10 +83,12 @@ function scheduleCronstyle(page: puppeteer.Page) {
   for (const key in task) {
     const item = task[key as keyof typeof task];
     schedule.scheduleJob(item.预约时间, async () => {
-      await checkInfo(page, item.url);
+      await checkInfo(page);
     });
     schedule.scheduleJob(item.抢购时间, async () => {
-      await checkInfo(page, item.url);
+      await reload(page, item.url);
+      await checkInfo(page);
+
       try {
         await page.waitForSelector('.checkout-submit');
         await page.evaluate(() => {
