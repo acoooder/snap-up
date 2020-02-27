92dc89d9fabc3d876e5e84496fe99669c618753a
summersky <summer.sky.2014@qq.com>
2020-02-27 20:12:13 +0800

    fix: 修复“无法查找到提交订单”的问题

--- src/index.ts
+++ src/index.ts
@@ -29,17 +29,20 @@ async function checkInfo(page: puppeteer.Page, url: string): Promise<void> {
   });
 
   return new Promise((resolve) => {
-    page.on('response', async (response) => {
+    const listener = async (response: puppeteer.Response) => {
       // 获取商品预约抢购信息的接口
       if (response.url().indexOf('https://yushou.jd.com/youshouinfo.action') >= 0) {
         const text = await response.text();
         const data = JSON.parse(text.slice(10, text.length - 2));
+
         // eslint-disable-next-line no-console
         console.log(data);
+        page.off('response', listener);
         await clickReservation(page);
         resolve(data.info);
       }
-    });
+    };
+    page.on('response', listener);
   });
 }
 
@@ -52,9 +55,9 @@ function scheduleCronstyle(page: puppeteer.Page) {
     schedule.scheduleJob(item.抢购时间, async () => {
       await checkInfo(page, item.url);
       try {
-        await page.waitForSelector('#checkout-submit');
+        await page.waitForSelector('.checkout-submit');
         await page.evaluate(() => {
-          const btn = document.querySelector('#checkout-submit') as HTMLButtonElement;
+          const btn = document.querySelector('.checkout-submit') as HTMLButtonElement;
           btn.click();
         });
       } catch (err) {
@@ -73,11 +76,16 @@ function scheduleCronstyle(page: puppeteer.Page) {
       height: 1080
     }
   });
+
   // 获取第一个标签页
   const page = (await browser.pages())[0];
-
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36');
   await page.goto('https://www.jd.com/');
   await login(page);
   scheduleCronstyle(page);
+  await page.waitForSelector('.checkout-submit');
+  await page.evaluate(() => {
+    const btn = document.querySelector('.checkout-submit') as HTMLButtonElement;
+    btn.click();
+  });
 })();
